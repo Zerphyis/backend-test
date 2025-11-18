bac57accef3d13ffb6f0f8af544ff4c28b0586c4
Otavio <estudosprogotavio@gmail.com>
2025-11-18 14:02:11 -0300

    TASK01: feat:adicionando ParkTestApplication.java

---
+++ src/main/java/dev/Zerphyis/ParkTest/ParkTestApplication.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.ParkTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Zerphyis/ParkTest/ParkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.ParkTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
