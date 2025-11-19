90462209c2856cfeb36cfb87074ca0c08bec8e26
Otavio <estudosprogotavio@gmail.com>
2025-11-19 13:39:47 -0300

    TASK01 feat:adicionando UseCase RegisterEntry

---
+++ src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/RegisterEntry.java
@@ -0,0 +1,18 @@
+package dev.Zerphyis.ParkTest.Aplication.UseCases;
+
+import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
+import dev.Zerphyis.ParkTest.Domain.repositorys.VehicleRepository;
+import dev.Zerphyis.ParkTest.Infra.EntityCore.Dtos.PlateVehicle;
+
+public class RegisterEntry {
+
+    private final VehicleRepository repository;
+
+    public RegisterEntry(VehicleRepository repository) {
+        this.repository = repository;
+    }
+
+    public VehicleDomain execute(PlateVehicle entry){
+        return repository.registerEntry(entry);
+    }
+}
