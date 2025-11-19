0451db9ab37dc4bebcb3cea396d7cb8c413355c9
Otavio <estudosprogotavio@gmail.com>
2025-11-19 13:41:04 -0300

    TASK01 feat:adicionando atributo ID em VehicleEntity

--- src/main/java/dev/Zerphyis/ParkTest/Infra/EntityCore/VehicleEntity.java
+++ src/main/java/dev/Zerphyis/ParkTest/Infra/EntityCore/VehicleEntity.java
@@ -1,33 +1,44 @@
-package dev.Zerphyis.ParkTest.Infra.EntityCore;
+    package dev.Zerphyis.ParkTest.Infra.EntityCore;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+    import dev.Zerphyis.ParkTest.Infra.EntityCore.TypeVehicle.TypesVehicles;
+    import jakarta.persistence.*;
+    import jakarta.validation.constraints.NotBlank;
+    import jakarta.validation.constraints.Pattern;
+    import lombok.AllArgsConstructor;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+    import lombok.Setter;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+    import java.math.BigDecimal;
+    import java.time.LocalDateTime;
 
-@NoArgsConstructor
-@Getter
-@Setter
-public class VehicleEntity {
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    public class VehicleEntity {
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Id
+        private  Long id;
 
-    @NotBlank
-    @Pattern(
-            regexp = "^[A-Z]{3}[0-9][A-Z][0-9]{2}$|^[A-Z]{3}-?[0-9]{4}$",
-            message = "Placa inválida. Use padrão antigo (AAA-1234) ou Mercosul (AAA1A23)."
-    )
-    private String plate;
+        @NotBlank
+        @Pattern(
+                regexp = "^[A-Z]{3}[0-9][A-Z][0-9]{2}$|^[A-Z]{3}-?[0-9]{4}$",
+                message = "Placa inválida. Use padrão antigo (AAA-1234) ou Mercosul (AAA1A23)."
+        )
+        private String plate;
 
-    private LocalDateTime entry;
-    private LocalDateTime exit;
-    private BigDecimal pending;
+        private LocalDateTime entry;
+        private LocalDateTime exit;
+        private BigDecimal pending;
 
-    public VehicleEntity(String plate) {
-        this.plate = plate.toUpperCase();
-        this.entry = LocalDateTime.now();
-        this.pending = BigDecimal.ZERO;
+        @Enumerated(EnumType.STRING)
+        private TypesVehicles typesVehicles;
+
+        public VehicleEntity(String plate) {
+            this.plate = plate.toUpperCase();
+            this.entry = LocalDateTime.now();
+            this.pending = BigDecimal.ZERO;
+
+        }
     }
-}
