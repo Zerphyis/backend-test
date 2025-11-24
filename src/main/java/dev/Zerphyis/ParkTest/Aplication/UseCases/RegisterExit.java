b346f57a009176864ce69d3575d0e92c41292e95
Otavio <estudosprogotavio@gmail.com>
2025-11-24 07:18:18 -0300

    TASK03 refactor:corrigindo lógica em UseCase RegisterExit

--- src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/RegisterExit.java
+++ src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/RegisterExit.java
@@ -3,6 +3,9 @@ package dev.Zerphyis.ParkTest.Aplication.UseCases;
 import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
 import dev.Zerphyis.ParkTest.Domain.repositorys.VehicleRepository;
 import dev.Zerphyis.ParkTest.Infra.EntityCore.Dtos.PlateVehicle;
+import dev.Zerphyis.ParkTest.Infra.Exceptions.VehiclePlateNotFoundException;
+
+import java.time.Duration;
 
 public class RegisterExit {
     private final VehicleRepository repository;
@@ -11,7 +14,19 @@ public class RegisterExit {
         this.repository = repository;
     }
 
-    public VehicleDomain execute(PlateVehicle plate) {
-        return repository.registerExit(plate);
+    public VehicleDomain execute(PlateVehicle plateDto) {
+        String plate= plateDto.plate();
+        VehicleDomain vehicle= repository.findByPlate(plate).orElseThrow(() -> new VehiclePlateNotFoundException("Veiculo Não encontrado"));
+
+        vehicle.assertHasEntry();
+        vehicle.markExitNow();
+
+
+        Duration visit = vehicle.currentVisitDuration();
+        vehicle.addAccumulated(visit);
+
+        vehicle.clearEntryExit();
+
+        return repository.save(vehicle);
     }
 }
