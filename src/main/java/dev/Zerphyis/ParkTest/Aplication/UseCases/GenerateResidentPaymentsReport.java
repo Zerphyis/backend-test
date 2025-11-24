20bf3881e6554920b85bdeb8c78845b52975dc03
Otavio <estudosprogotavio@gmail.com>
2025-11-24 07:18:55 -0300

    TASK03 refactor:corrigindo lógica em GenerateResidentPaymentsReport

--- src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/GenerateResidentPaymentsReport.java
+++ src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/GenerateResidentPaymentsReport.java
@@ -1,15 +1,41 @@
 package dev.Zerphyis.ParkTest.Aplication.UseCases;
 
+import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
+import dev.Zerphyis.ParkTest.Domain.Enums.TypesVehicles;
+import dev.Zerphyis.ParkTest.Domain.Interfaces.ResidentPaymentRepository;
 import dev.Zerphyis.ParkTest.Domain.repositorys.VehicleRepository;
 
-public class GenerateResidentPaymentsReport {
-    private final VehicleRepository repository;
+import java.util.List;
 
-    public GenerateResidentPaymentsReport(VehicleRepository repository) {
-        this.repository = repository;
+public class GenerateResidentPaymentsReport {
+
+    private final VehicleRepository vehicleRepository;
+    private final ResidentPaymentRepository residentPaymentRepository;
+
+    public GenerateResidentPaymentsReport(
+            VehicleRepository vehicleRepository,
+            ResidentPaymentRepository residentPaymentRepository
+    ) {
+        this.vehicleRepository = vehicleRepository;
+        this.residentPaymentRepository = residentPaymentRepository;
     }
 
     public void execute(String filename) {
-        repository.generateResidentPayments(filename);
+
+        List<VehicleDomain> residents = vehicleRepository.findAll(0).stream()
+                .filter(v -> v.getType() == TypesVehicles.RESIDENTS)
+                .toList();
+
+        StringBuilder report = new StringBuilder();
+        report.append("Relatório de Pagamentos dos Residentes\n\n");
+
+        for (VehicleDomain resident : residents) {
+            report.append("Placa: ").append(resident.getPlate()).append("\n");
+            report.append("Minutos acumulados: ").append(resident.getAccumulatedTime().toMinutes()).append("\n");
+            report.append("Valor devido: ").append(resident.getPending()).append("\n");
+            report.append("---------------------------\n");
+        }
+
+        residentPaymentRepository.saveMonthlyReport(filename, report.toString());
     }
 }
