ab2f05105015d447b0e24cf3ca7c9121211e3423
Otavio <estudosprogotavio@gmail.com>
2025-11-24 07:18:07 -0300

    TASK03 refactor:corrigindo lógica em UseCase RegisterEntry

--- src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/RegisterEntry.java
+++ src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/RegisterEntry.java
@@ -3,6 +3,7 @@ package dev.Zerphyis.ParkTest.Aplication.UseCases;
 import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
 import dev.Zerphyis.ParkTest.Domain.repositorys.VehicleRepository;
 import dev.Zerphyis.ParkTest.Infra.EntityCore.Dtos.PlateVehicle;
+import dev.Zerphyis.ParkTest.Infra.Exceptions.VehiclePlateNotFoundException;
 
 public class RegisterEntry {
 
@@ -12,7 +13,13 @@ public class RegisterEntry {
         this.repository = repository;
     }
 
-    public VehicleDomain execute(PlateVehicle plate) {
-        return repository.registerEntry(plate);
+    public VehicleDomain execute(PlateVehicle plateDto) {
+        String plate = plateDto.plate().toUpperCase();
+        VehicleDomain vehicle =repository.findByPlate(plate).orElseThrow(() -> new VehiclePlateNotFoundException("Veículo não encontrado"));
+
+        vehicle.assertNotInside();
+        vehicle.markEntryNow();
+
+        return repository.save(vehicle);
     }
 }
