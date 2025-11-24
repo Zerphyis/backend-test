0c510179c1dfa0d47c3673facdde061b9d4fcbe5
Otavio <estudosprogotavio@gmail.com>
2025-11-24 07:19:28 -0300

    TASK03 delete:deletando Classe PlateNotFound.java

--- src/main/java/dev/Zerphyis/ParkTest/Infra/Exceptions/PlateNotFound.java
+++
@@ -1,7 +0,0 @@
-package dev.Zerphyis.ParkTest.Infra.Exceptions;
-
-public class PlateNotFound extends RuntimeException {
-    public PlateNotFound(String message) {
-        super(message);
-    }
-}
