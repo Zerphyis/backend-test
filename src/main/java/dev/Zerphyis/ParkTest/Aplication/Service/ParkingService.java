4854e00b5ad030e04573923f0350b21791d70a36
Otavio <estudosprogotavio@gmail.com>
2025-11-25 08:46:55 -0300

    TASK04 refactor: adicionando UseCases novos em ParkingService

--- src/main/java/dev/Zerphyis/ParkTest/Aplication/Service/ParkingService.java
+++ src/main/java/dev/Zerphyis/ParkTest/Aplication/Service/ParkingService.java
@@ -2,10 +2,12 @@ package dev.Zerphyis.ParkTest.Aplication.Service;
 
 import dev.Zerphyis.ParkTest.Aplication.UseCases.*;
 import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
+import dev.Zerphyis.ParkTest.Domain.Enums.TypesVehicles;
 import dev.Zerphyis.ParkTest.Infra.EntityCore.Dtos.PlateVehicle;
-import org.springframework.stereotype.Service;
 
-@Service
+import java.util.List;
+import java.util.Optional;
+
 public class ParkingService {
 
     private final RegisterEntry registerEntry;
@@ -13,17 +15,32 @@ public class ParkingService {
     private final AddOfficialVehicle addOfficialVehicle;
     private final AddResidentVehicle addResidentVehicle;
     private final StartMonth startMonth;
+    private final FindAllVehicles findAllVehicles;
+    private final FindVehiclesByTypes findVehiclesByTypes;
+    private final FindVehicleByPlate findVehicleByPlate;
     private final GenerateResidentPaymentsReport generateReport;
 
-    public ParkingService(RegisterEntry registerEntry, RegisterExit registerExit, AddOfficialVehicle addOfficialVehicle, AddResidentVehicle addResidentVehicle, StartMonth startMonth, GenerateResidentPaymentsReport generateReport) {
+    public ParkingService(
+            RegisterEntry registerEntry,
+            RegisterExit registerExit,
+            AddOfficialVehicle addOfficialVehicle,
+            AddResidentVehicle addResidentVehicle,
+            StartMonth startMonth,
+            FindAllVehicles findAllVehicles,
+            FindVehiclesByTypes findVehiclesByTypes,
+            FindVehicleByPlate findVehicleByPlate,
+            GenerateResidentPaymentsReport generateReport
+    ) {
         this.registerEntry = registerEntry;
         this.registerExit = registerExit;
         this.addOfficialVehicle = addOfficialVehicle;
         this.addResidentVehicle = addResidentVehicle;
         this.startMonth = startMonth;
+        this.findAllVehicles = findAllVehicles;
+        this.findVehiclesByTypes = findVehiclesByTypes;
+        this.findVehicleByPlate = findVehicleByPlate;
         this.generateReport = generateReport;
     }
-
 
     public VehicleDomain registerEntry(PlateVehicle plate) {
         return registerEntry.execute(plate);
@@ -48,6 +65,16 @@ public class ParkingService {
     public void generateResidentReport(String filename) {
         generateReport.execute(filename);
     }
+
+    public List<VehicleDomain> getAllVehicles(int page) {
+        return findAllVehicles.execute(page);
+    }
+
+    public Optional<VehicleDomain> getVehicleByPlate(String plate) {
+        return findVehicleByPlate.execute(plate);
+    }
+
+    public List<VehicleDomain> getVehiclesByType(TypesVehicles type) {
+        return findVehiclesByTypes.execute(type);
+    }
 }
-
-
