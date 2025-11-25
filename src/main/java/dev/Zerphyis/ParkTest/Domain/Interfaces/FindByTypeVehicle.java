f5952671942a1578ffc745d82df25d177b8919bf
Otavio <estudosprogotavio@gmail.com>
2025-11-25 08:45:39 -0300

    TASK04 feat:adicionando UseCase FindByTypeVehicle

---
+++ src/main/java/dev/Zerphyis/ParkTest/Domain/Interfaces/FindByTypeVehicle.java
@@ -0,0 +1,10 @@
+package dev.Zerphyis.ParkTest.Domain.Interfaces;
+
+import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
+import dev.Zerphyis.ParkTest.Domain.Enums.TypesVehicles;
+
+import java.util.List;
+
+public interface FindByTypeVehicle {
+    List<VehicleDomain> execute(TypesVehicles type);
+}
