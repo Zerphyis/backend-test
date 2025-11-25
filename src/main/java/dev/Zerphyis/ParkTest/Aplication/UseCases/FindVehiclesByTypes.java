ee29c43e4d7bec95af5065b4c810061c8e90ef1a
Otavio <estudosprogotavio@gmail.com>
2025-11-25 08:45:57 -0300

    TASK04 feat:adicionando UseCase FindVehiclesByTypes

---
+++ src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/FindVehiclesByTypes.java
@@ -0,0 +1,24 @@
+package dev.Zerphyis.ParkTest.Aplication.UseCases;
+
+import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
+import dev.Zerphyis.ParkTest.Domain.Enums.TypesVehicles;
+import dev.Zerphyis.ParkTest.Domain.Interfaces.FindByTypeVehicle;
+import dev.Zerphyis.ParkTest.Domain.repositorys.VehicleRepository;
+
+import java.util.List;
+
+public class FindVehiclesByTypes implements FindByTypeVehicle {
+
+    private final VehicleRepository repository;
+
+    public FindVehiclesByTypes(VehicleRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<VehicleDomain> execute(TypesVehicles type) {
+        return repository.findAll(0).stream()
+                .filter(vehicle -> vehicle.getType() == type)
+                .toList();
+    }
+}
