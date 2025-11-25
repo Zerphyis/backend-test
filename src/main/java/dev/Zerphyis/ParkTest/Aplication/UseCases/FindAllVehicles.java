85dce3d00f4303bceafc52d1329ce8f7bd50e402
Otavio <estudosprogotavio@gmail.com>
2025-11-25 08:45:28 -0300

    TASK04 feat:adicionando UseCase FindAllVehicles

---
+++ src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/FindAllVehicles.java
@@ -0,0 +1,19 @@
+package dev.Zerphyis.ParkTest.Aplication.UseCases;
+
+import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
+import dev.Zerphyis.ParkTest.Domain.repositorys.VehicleRepository;
+
+import java.util.List;
+
+public class FindAllVehicles {
+
+    private final VehicleRepository repository;
+
+    public FindAllVehicles(VehicleRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<VehicleDomain> execute(int page) {
+        return repository.findAll(page);
+    }
+}
