bd5e49b97ccb5fd8d01e489497ea7e52d1184369
Otavio <estudosprogotavio@gmail.com>
2025-11-27 01:33:50 -0300

    TASK05 refactor:VehicleRepositoryJpa corrigindo query para o banco

--- src/main/java/dev/Zerphyis/ParkTest/Domain/repositorys/jpa/VehicleRepositoryJpa.java
+++ src/main/java/dev/Zerphyis/ParkTest/Domain/repositorys/jpa/VehicleRepositoryJpa.java
@@ -4,13 +4,16 @@ import dev.Zerphyis.ParkTest.Infra.EntityCore.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-public interface VehicleRepositoryJpa extends JpaRepository<VehicleEntity,Long> {
+public interface VehicleRepositoryJpa extends JpaRepository<VehicleEntity, Long> {
+
     Optional<VehicleEntity> findByPlate(String plate);
 
     @Modifying
-    @Query("UPDATE VehicleEntity v SET v.totalStays = 0 WHERE v.type = 'OFFICIAL'")
-    void deleteAllStaysOfOfficialVehicles();
+    @Transactional
+    @Query("UPDATE VehicleEntity v SET v.accumulatedTimeSeconds = 0, v.pending = 0 WHERE v.type = 'OFFICIAL'")
+    void resetOfficialVehiclesMonth();
 }
