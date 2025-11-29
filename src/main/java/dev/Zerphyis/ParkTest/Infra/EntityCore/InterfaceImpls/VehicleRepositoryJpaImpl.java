4a387a3c656282eed759ef4478f66c5082fb608d
Otavio <estudosprogotavio@gmail.com>
2025-11-29 02:45:10 -0300

    TASK06 refactor:mudando tipagem de metodos em VehicleRepositoryJpaImpl

--- src/main/java/dev/Zerphyis/ParkTest/Infra/EntityCore/InterfaceImpls/VehicleRepositoryJpaImpl.java
+++ src/main/java/dev/Zerphyis/ParkTest/Infra/EntityCore/InterfaceImpls/VehicleRepositoryJpaImpl.java
@@ -5,18 +5,17 @@ import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
 import dev.Zerphyis.ParkTest.Domain.repositorys.VehicleRepository;
 import dev.Zerphyis.ParkTest.Domain.repositorys.jpa.VehicleRepositoryJpa;
 import dev.Zerphyis.ParkTest.Infra.EntityCore.VehicleEntity;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
-@Repository
 public class VehicleRepositoryJpaImpl implements VehicleRepository {
 
     private final VehicleRepositoryJpa jpa;
 
-    public VehicleRepositoryJpaImpl(@Lazy VehicleRepositoryJpa jpa) {
+    public VehicleRepositoryJpaImpl(VehicleRepositoryJpa jpa) {
         this.jpa = jpa;
     }
 
@@ -27,16 +26,17 @@ public class VehicleRepositoryJpaImpl implements VehicleRepository {
     }
 
     @Override
-    public List<VehicleDomain> findAll(int page) {
-        return jpa.findAll().stream()
-                .map(VehicleMapper::toDomain)
-                .toList();
+    public Page<VehicleDomain> findAll(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return jpa.findAll(pageable)
+                .map(VehicleMapper::toDomain);
     }
 
     @Override
     public VehicleDomain save(VehicleDomain vehicle) {
         VehicleEntity entity = VehicleMapper.toEntity(vehicle);
-        return VehicleMapper.toDomain(jpa.save(entity));
+        VehicleEntity saved = jpa.save(entity);
+        return VehicleMapper.toDomain(saved);
     }
 
     @Override
