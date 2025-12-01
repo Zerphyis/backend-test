308be22ce86aeaa23e024454c085b57ca3cf3017
Otavio <estudosprogotavio@gmail.com>
2025-12-01 14:19:15 -0300

    TASK07 feat:deixando o pageFix em UseCases FindVehiclesByTypes

--- src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/FindVehiclesByTypes.java
+++ src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/FindVehiclesByTypes.java
@@ -5,14 +5,12 @@ import dev.Zerphyis.ParkTest.Domain.Enums.TypesVehicles;
 import dev.Zerphyis.ParkTest.Domain.Interfaces.FindByTypeVehicle;
 import dev.Zerphyis.ParkTest.Domain.repositorys.VehicleRepository;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 
-import java.util.List;
 
 public class FindVehiclesByTypes implements FindByTypeVehicle {
 
     private final VehicleRepository repository;
+    private static final int PageFix = 4;
 
     public FindVehiclesByTypes(VehicleRepository repository) {
         this.repository = repository;
@@ -21,13 +19,7 @@ public class FindVehiclesByTypes implements FindByTypeVehicle {
     @Override
     public Page<VehicleDomain> execute(TypesVehicles type, int page, int size) {
 
-        Page<VehicleDomain> fullPage = repository.findAll(page, size);
-
-        List<VehicleDomain> filtered = fullPage.getContent().stream()
-                .filter(v -> v.getType() == type)
-                .toList();
-
-        return new PageImpl<>(filtered, PageRequest.of(page, size), filtered.size());
+        return repository.findByType(type, PageFix, size);
     }
 
 
