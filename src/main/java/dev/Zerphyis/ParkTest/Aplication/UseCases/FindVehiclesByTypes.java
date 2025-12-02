ea5d36a6f05c85e9b18c108b681a1bb3035ab370
Otavio <estudosprogotavio@gmail.com>
2025-12-02 05:47:27 -0300

    TASK07 refactor:removendo page em FindVehiclesByTypes

--- src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/FindVehiclesByTypes.java
+++ src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/FindVehiclesByTypes.java
@@ -4,23 +4,19 @@ import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
 import dev.Zerphyis.ParkTest.Domain.Enums.TypesVehicles;
 import dev.Zerphyis.ParkTest.Domain.Interfaces.FindByTypeVehicle;
 import dev.Zerphyis.ParkTest.Domain.repositorys.VehicleRepository;
-import org.springframework.data.domain.Page;
 
+import java.util.List;
 
 public class FindVehiclesByTypes implements FindByTypeVehicle {
 
     private final VehicleRepository repository;
-    private static final int PageFix = 4;
 
     public FindVehiclesByTypes(VehicleRepository repository) {
         this.repository = repository;
     }
 
     @Override
-    public Page<VehicleDomain> execute(TypesVehicles type, int page, int size) {
-
-        return repository.findByType(type, PageFix, size);
+    public List<VehicleDomain> execute(TypesVehicles type) {
+        return repository.findByType(type);
     }
-
-
 }
