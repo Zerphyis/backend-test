6171d7622e8e6f9d71a072f8731a46669872832e
Otavio <estudosprogotavio@gmail.com>
2025-12-02 05:43:09 -0300

    TASK07 feat:adicionando ID em VehicleDomain

--- src/main/java/dev/Zerphyis/ParkTest/Domain/Entity/VehicleDomain.java
+++ src/main/java/dev/Zerphyis/ParkTest/Domain/Entity/VehicleDomain.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 
 public class VehicleDomain {
 
+    private Long id; // novo campo
     private final String plate;
     private TypesVehicles type;
 
@@ -19,7 +20,9 @@ public class VehicleDomain {
     private Duration accumulatedTime;
     private BigDecimal pending;
 
+    // construtor sem ID (para criação de novos veículos)
     public VehicleDomain(String plate, TypesVehicles type) {
+        this.id = null;
         this.plate = Objects.requireNonNull(plate).toUpperCase();
         this.type = Objects.requireNonNull(type);
         this.entry = null;
@@ -28,20 +31,18 @@ public class VehicleDomain {
         this.pending = BigDecimal.ZERO;
     }
 
-
-    public VehicleDomain(String plate,
+    public VehicleDomain(Long id,
+                         String plate,
                          TypesVehicles type,
                          LocalDateTime entry,
                          LocalDateTime exit,
                          Duration accumulatedTime,
                          BigDecimal pending) {
-
+        this.id = id;
         this.plate = Objects.requireNonNull(plate).toUpperCase();
         this.type = Objects.requireNonNull(type);
-
         this.entry = entry;
         this.exit = exit;
-
         this.accumulatedTime = accumulatedTime == null ? Duration.ZERO : accumulatedTime;
         this.pending = pending == null ? BigDecimal.ZERO : pending;
     }
@@ -92,14 +93,13 @@ public class VehicleDomain {
         this.pending = pending == null ? BigDecimal.ZERO : pending;
     }
 
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
     public String getPlate() { return plate; }
     public LocalDateTime getEntry() { return entry; }
     public LocalDateTime getExit() { return exit; }
     public Duration getAccumulatedTime() { return accumulatedTime; }
     public BigDecimal getPending() { return pending; }
     public TypesVehicles getType() { return type; }
-
-    public void setType(TypesVehicles t) {
-        this.type = t;
-    }
+    public void setType(TypesVehicles t) { this.type = t; }
 }
