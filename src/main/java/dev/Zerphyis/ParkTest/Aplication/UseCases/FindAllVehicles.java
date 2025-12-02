d6520a31bb8c8686afdb60d1768a66f47bbade7c
Otavio <estudosprogotavio@gmail.com>
2025-12-02 05:47:10 -0300

    TASK07 refactor:removendo page em FindAllVehicles

--- src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/FindAllVehicles.java
+++ src/main/java/dev/Zerphyis/ParkTest/Aplication/UseCases/FindAllVehicles.java
@@ -2,18 +2,18 @@ package dev.Zerphyis.ParkTest.Aplication.UseCases;
 
 import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
 import dev.Zerphyis.ParkTest.Domain.repositorys.VehicleRepository;
-import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public class FindAllVehicles {
 
     private final VehicleRepository repository;
-    private static final int PageFix = 4;
 
     public FindAllVehicles(VehicleRepository repository) {
         this.repository = repository;
     }
 
-    public Page<VehicleDomain> execute(int page, int size) {
-        return repository.findAll(PageFix, size);
+    public List<VehicleDomain> execute() {
+        return repository.findAll();
     }
 }
