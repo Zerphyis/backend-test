0a5b8825838c7897d391f19434e0013ab4a2bcb3
Otavio <estudosprogotavio@gmail.com>
2025-12-10 02:31:56 -0300

    TASK08 feat:adicionando ChacheAside em ParkingService

--- src/main/java/dev/Zerphyis/ParkTest/Aplication/Service/ParkingService.java
+++ src/main/java/dev/Zerphyis/ParkTest/Aplication/Service/ParkingService.java
@@ -4,9 +4,14 @@ import dev.Zerphyis.ParkTest.Aplication.UseCases.*;
 import dev.Zerphyis.ParkTest.Domain.Entity.VehicleDomain;
 import dev.Zerphyis.ParkTest.Domain.Enums.TypesVehicles;
 import dev.Zerphyis.ParkTest.Infra.EntityCore.Dtos.PlateVehicle;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 
 import java.util.List;
 import java.util.Optional;
+
+import static dev.Zerphyis.ParkTest.Domain.Enums.CurrencyType.BRL;
 
 public class ParkingService {
 
@@ -42,38 +47,68 @@ public class ParkingService {
         this.generateReportUseCase = generateReport;
     }
 
+    @Caching(evict = {
+            @CacheEvict(value = "vehicles_all", allEntries = true),
+            @CacheEvict(value = "vehicles_by_plate", key = "#plate.plate"),
+            @CacheEvict(value = "vehicles_by_type", allEntries = true)
+    })
     public VehicleDomain registerEntry(PlateVehicle plate) {
         return registerEntryUseCase.execute(plate);
     }
 
+    @Caching(evict = {
+            @CacheEvict(value = "vehicles_all", allEntries = true),
+            @CacheEvict(value = "vehicles_by_plate", key = "#plate.plate"),
+            @CacheEvict(value = "vehicles_by_type", allEntries = true)
+    })
     public VehicleDomain registerExit(PlateVehicle plate) {
         return registerExitUseCase.execute(plate);
     }
 
+    @Caching(evict = {
+            @CacheEvict(value = "vehicles_all", allEntries = true),
+            @CacheEvict(value = "vehicles_by_plate", key = "#plate.plate"),
+            @CacheEvict(value = "vehicles_by_type", allEntries = true)
+    })
     public VehicleDomain addResident(PlateVehicle plate) {
         return addResidentVehicleUseCase.execute(plate);
     }
 
+    @Caching(evict = {
+            @CacheEvict(value = "vehicles_all", allEntries = true),
+            @CacheEvict(value = "vehicles_by_plate", key = "#plate.plate"),
+            @CacheEvict(value = "vehicles_by_type", allEntries = true)
+    })
     public VehicleDomain addOfficial(PlateVehicle plate) {
         return addOfficialVehicleUseCase.execute(plate);
     }
 
+    @Caching(evict = {
+            @CacheEvict(value = "vehicles_all", allEntries = true),
+            @CacheEvict(value = "vehicles_by_plate", allEntries = true),
+            @CacheEvict(value = "vehicles_by_type", allEntries = true)
+    })
     public void startNewMonth() {
         startMonthUseCase.execute();
     }
 
     public void generateResidentReport(String filename) {
-        generateReportUseCase.execute(filename, dev.Zerphyis.ParkTest.Domain.Enums.CurrencyType.BRL);
+        generateReportUseCase.execute(filename, BRL);
     }
 
+
+
+    @Cacheable("vehicles_all")
     public List<VehicleDomain> getAllVehicles() {
         return findAllVehiclesUseCase.execute();
     }
 
+    @Cacheable(value = "vehicles_by_plate", key = "#plate")
     public Optional<VehicleDomain> getVehicleByPlate(String plate) {
         return findVehicleByPlateUseCase.execute(plate);
     }
 
+    @Cacheable(value = "vehicles_by_type", key = "#type")
     public List<VehicleDomain> getVehiclesByType(TypesVehicles type) {
         return findVehiclesByTypesUseCase.execute(type);
     }
